4fcce319c25786a7e920bd5f1742113db4542c19
John Oscar Molteberg <johnoscarm@gmail.com>
2023-04-22 18:58:34 +0200

    Some changes to searchbar(not functionable)

--- js/ui/navbar.js
+++ js/ui/navbar.js
@@ -6,15 +6,26 @@ const popupSearchInput = document.getElementById('popup-search-input');
 searchButton.addEventListener('click', function() {
     searchPopup.style.display = 'block';
     searchPopup.style.position = 'absolute';
-    searchPopup.style.top = '100%';
+    searchPopup.style.top = '0';
+    searchPopup.style.transition = 'all 0.3s ease-in-out';
+ 
+  const mediaQuery = window.matchMedia('(max-width: 800px)');
+
+  if (mediaQuery.matches) {
+    searchPopup.style.top = '43px';
+  } else {
+    searchPopup.style.top = '0';
+  }
+
+  if (mediaQuery.matches) {
+    searchPopup.style.left = '-255px';
+  } else {
     searchPopup.style.left = '0';
-  });
+  }
+});
 
 closeButton.addEventListener('click', function() {
   searchPopup.style.display = 'none';
 });
 
-popupSearchInput.addEventListener('input', function() {
-  const searchText = popupSearchInput.value.toLowerCase();
-  // Perform the search functionality here
-});
+
